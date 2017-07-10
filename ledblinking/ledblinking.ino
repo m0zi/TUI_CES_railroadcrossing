e338fcd83e9015df44d10d3b3a509bc6d626aa39
Syed Muhammad Ali Azher <smaliazher@hotmail.com>
2017-07-10 21:16:35 +0200

    Two led blinking with gate
    LED blinking

---
+++ ledblinking/ledblinking.ino
@@ -0,0 +1,13 @@
+void setup(){
+pinMode(8,OUTPUT);
+pinMode(12,OUTPUT);
+}
+
+void loop(){
+digitalWrite(8,HIGH);
+digitalWrite(12,LOW);
+delay(150);
+digitalWrite(8,LOW);
+digitalWrite(12,HIGH);
+delay(150);
+}
