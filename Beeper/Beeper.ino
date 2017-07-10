aca13ea85d4774a8b2a1b47d07cadffa8710012a
m0zi <m.remmos@googlemail.com>
2017-07-10 20:29:28 +0200

    buzzer working
    Buzzer is working now, have to plug in 5V into + and signal with resistor on S though.

--- Beeper/Beeper.ino
+++ Beeper/Beeper.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+const int BUZZER_PIN = 9;
 
+void setup() {
+  // set buzzer pin to output mode
+  pinMode(BUZZER_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  tone(BUZZER_PIN, 1000); // send 1kHz signal...
+  delay(250);            // ... for 1 sec...
+  noTone(BUZZER_PIN);     // stop sound...
+  delay(250);            // ... for 1 sec
 }
