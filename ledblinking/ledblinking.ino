f377e5def0a72b413f80780bf19a950d99e55a88
Syed Muhammad Ali Azher <smaliazher@hotmail.com>
2017-07-10 21:22:50 +0200

    Blinking pin 11 and 12
    LED BLINKING

--- ledblinking/ledblinking.ino
+++ ledblinking/ledblinking.ino
@@ -1,13 +1,13 @@
 void setup(){
-pinMode(8,OUTPUT);
+pinMode(11,OUTPUT);
 pinMode(12,OUTPUT);
 }
 
 void loop(){
-digitalWrite(8,HIGH);
+digitalWrite(11,HIGH);
 digitalWrite(12,LOW);
 delay(150);
-digitalWrite(8,LOW);
+digitalWrite(11,LOW);
 digitalWrite(12,HIGH);
 delay(150);
 }
